a8600e23bb444d100515309fc55d02eae392c425
Alexey Mikhailov <alexer_78@mail.ru>
2022-06-19 00:34:37 +0300

    The save(), get(), delete(), clear(), getAll() and size() methods are implemented in the ArrayStrage class

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -4,27 +4,91 @@
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
+    /**
+     * clear all array storage
+     */
     void clear() {
+        int arrayNumber = 0;
+        while ((!(storage[arrayNumber] == null)) && (arrayNumber < storage.length)) {
+            storage[arrayNumber] = null;
+            arrayNumber++;
+        }
     }
 
+    /**
+     * add new member to array Resume[] storage
+     */
     void save(Resume r) {
+        int arrayNumber = 0;
+        while (!(storage[arrayNumber] == null)) {
+            arrayNumber++;
+        }
+
+        storage[arrayNumber] = r;
     }
 
+    /**
+     * return member from array Resume[] storage by the specified unique number
+     */
     Resume get(String uuid) {
+        int arrayNumber = 0;
+        while ((!(storage[arrayNumber] == null)) && (arrayNumber < storage.length)) {
+            if (storage[arrayNumber].uuid == uuid) return storage[arrayNumber];
+            arrayNumber++;
+        }
+
         return null;
     }
 
+    /**
+     * delete member from array Resume[] storage by the specified unique number
+     */
     void delete(String uuid) {
+        int arrayNumber = 0;
+        while ((!(storage[arrayNumber] == null)) && (arrayNumber < storage.length)) {
+            if (storage[arrayNumber].uuid == uuid) break;
+            arrayNumber++;
+        }
+
+        while ((!(storage[arrayNumber] == null)) && (arrayNumber < storage.length)) {
+            storage[arrayNumber] = storage[arrayNumber + 1];
+            arrayNumber++;
+        }
+
+        storage[arrayNumber + 1] = null;
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        int arrayNumber = 0;
+        while ((!(storage[arrayNumber] == null)) && (arrayNumber < storage.length)) {
+            arrayNumber++;
+        }
+
+        int arraySize = arrayNumber;
+        Resume[] newStorage = new Resume[arrayNumber];
+
+        for (int i = 0; i < arraySize; i++) {
+            newStorage[i] = storage[i];
+        }
+
+        return newStorage;
+
     }
 
+    /**
+     * return lenght of array, contains only Resumes in storage (without null)
+     */
     int size() {
-        return 0;
+
+        int arrayLenght = 0;
+        while ((!(storage[arrayLenght] == null)) && (arrayLenght < storage.length)) {
+            arrayLenght++;
+        }
+
+        return arrayLenght;
     }
 }
